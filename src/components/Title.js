d4e82ac1e33708fa6718ae728c6cb19d9b869296
Gustavo Abreu <gustavofart371@gmail.com>
2022-05-13 16:52:24 -0300

    teste

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return <h2>{headline}</h2>;
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+
+Title.defaultProps = {
+  headline: 'Planetas',
+};
+
+export default Title;
